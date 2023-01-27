a59ab5bbd4bf7b6e90dac3bf9f79cd82406b086d
sumitbharti93 <sumitbharti93@gmail.com>
2023-01-27 10:29:46 +0530

    basic flask app deployed

---
+++ flasksimple.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author' : 'Sumit Bharti',
+        'title' : 'web development',
+        'content' : 'First post',
+        'date_posted' : '26 Jan 2023'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
